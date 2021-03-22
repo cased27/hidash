2c297052144a99285dd5f767794d5dd31d70fdd9
Casey D <caseydasilva@hotmail.com>
2021-03-22 12:42:30 -0700

    small refactor using 'assert' library

--- index.test.js
+++ index.test.js
@@ -1,3 +1,4 @@
+const assert = require('assert');
 const { forEach, map } = require('./index');
 
 const test = (desc, fn) => {
@@ -14,23 +15,17 @@ test('The forEach Func', () => {
     forEach([1, 2, 3], (value) => {
         sum += value;
     });
-    if (sum !== 6) {
-        throw new Error('Expecting summing array to equal 6');
-    }
+    assert.strictEqual(sum, 6, 'Expected forEach to sum the array');
 });
 
 test('The Map Func', () => {
     const result = map([1, 2, 3], value => {
         return value * 2;
     });
-    if (result[0] !== 2) {
-        throw new Error(`Expected to find 2, but found ${result[0]}`)
-    }
-    if (result[1] !== 4) {
-        throw new Error(`Expected to find 4, but found ${result[0]}`)
-    }
-    if (result[2] !== 6) {
-        throw new Error(`Expected to find 6, but found ${result[0]}`)
-    }
+
+    assert.strictEqual(result[0], 2)
+    assert.strictEqual(result[1], 4)
+    assert.strictEqual(result[2], 6)
+    // assert.deepStrictEqual(result, [2,4,6]);
 });
 
